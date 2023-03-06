3db62813f7f3b3d56233ff4c5399ed4f15c2feac
Mainakh Sarkar <mainakh.sarkar@hotmail.com>
2023-03-06 18:18:02 +0530

    Added Dev Mode.

--- src/config_manager.py
+++ src/config_manager.py
@@ -1,19 +1,21 @@
 import configparser
 
+
 def create_default_config():
     config_file = configparser.ConfigParser()
 
     config_file.add_section("Limits")
+    config_file.add_section("Dev")
 
     config_file.set("Limits", "folder_name_limit", "30")
     config_file.set("Limits", "save_recently_copied_passwords", "YES")
     config_file.set("Limits", "number_of_passwords_to_save", "8")
+    config_file.set("Dev", "password_check", "0")
 
     with open(r"config.ini", 'w') as config_file_obj:
         config_file.write(config_file_obj)
         config_file_obj.flush()
         config_file_obj.close()
-
 
 
 """        
